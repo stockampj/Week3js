dcff1353858074ae58e924b7879be91585821933
Joel Stockamp <stockampj@gmail.com>
2019-08-23 10:16:19 -0700

    add filter to replace elements of number array with appropriate message for numbers containing 3,2,1

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,35 @@ $(document).ready(function() {
     event.preventDefault();
     $(".display").text("");
 
-//Text Cleaner
+    //Text Cleaner
     var regex = /[^0-9-]/g
     var userInput = parseInt($("#userNumber").val().replace(regex, ""));
 
-//Display Text
+    //Display Text
     var output = numberValidator(userInput)
     $(".display").text(output)
 
     function numberValidator(number) {
-       var check = Number.isInteger(number);
-       console.log(check);
-       return check;
+      var check = Number.isInteger(number);
+      console.log(check);
+      return check;
     };
 
     var initialArray = []
+
     function arrayPopulator(number) {
-      for (var i=0; i<=userInput; i++) {
+      for (var i = 0; i <= userInput; i++) {
         var element = i.toString();
+        var digets = element.split("");
+        if (digets.indexOf("3")>=0) {
+          element = "I'm sorry, Dave. I'm afraid I can't do that.";
+        } else if (digets.indexOf("2")>=0) {
+          element = "Boop";
+        } else if (digets.indexOf("1")>=0) {
+          element = "Beep";
+        } else {
+          element = i;
+        }
         initialArray.push(element);
       }
 
@@ -28,7 +39,7 @@ $(document).ready(function() {
 
 
 
-//Director
+    //Director
     arrayPopulator(userInput)
     console.log(initialArray);
 
