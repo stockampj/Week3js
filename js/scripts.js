b272b0c04eb575e44883c7d89a5fc60b55080f86
Joel Stockamp <stockampj@gmail.com>
2019-08-23 12:25:59 -0700

    add a filter to display the IM SORRY DAVE text only once in a sequence to reduce its presence on the screen

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,20 @@ $(document).ready(function() {
     var regex = /[^0-9-]/g
     var userInput = parseInt($("#userNumber").val().replace(regex, ""));
 
+    //Display Text
+    outputProcessor(userInput).forEach(function(number) {
+      if (number === "error3") {
+        $(".display").append("<span id=\"error3\">" + "I'm s0rRy, Dave. I'm afR@id I caN't d0 tHat!  " + "</span>");
+      } else if (number === "error2") {
+        $(".display").append("b00p  ");
+      } else if (number === "error1") {
+        $(".display").append("BeeP  ");
+      } else {
+        $(".display").append(number + "  ");
+      }
+    })
 
-    function numberValidator(number) {
-      var check = Number.isInteger(number);
-      console.log(check);
-      return check;
-    };
-
-
+    var hexPicker = ["#f0e6bd;", "#ccbb76;", "#ccb03f;", "#d1a800;", "#b7bd99;", "#b7bd99;", "#899e20;", "#8f9e88;", "#8bd16b;", "#3eab0c;"]
 
     function outputProcessor(number) {
       var outputArray = []
@@ -32,51 +38,53 @@ $(document).ready(function() {
         }
         outputArray.push(element);
       }
+      for (var i = 0; i < (outputArray.length-1); i++){
+        if ((outputArray[i] === "error3") && (outputArray[i+1] === "error3")) {
+          outputArray[i] = "";
+        }
+      }
       return outputArray
     };
 
-    outputProcessor(userInput).forEach(function(number) {
-      if (number === "error3") {
-        $(".display").append("<span id=\"error3\">" + "I'm s0rRy, D@ve. I'm afR@id I caN't d0 tHat " + "</span>");
-      } else if (number === "error2") {
-        $(".display").append("b00p ");
-      } else if (number === "error1") {
-        $(".display").append("BeeP ");
-      } else {
-        $(".display").append(number+" ");
-      }
-    })
 
-
-
-
-
-
-    // function outputProcessor(number) {
-    //   var outputString = ""
-    //   for (var i = 0; i <= userInput; i++) {
-    //     var element = i.toString();
-    //     var digets = element.split("");
-    //     if (digets.indexOf("3") >= 0) {
-    //       element = "<span id=\"error3\">" + "I'm sorry, Dave. I'm afraid I can't do that." + "</span>";
-    //     } else if (digets.indexOf("2") >= 0) {
-    //       element = "<span id=\"error2\">Boop </span>";
-    //     } else if (digets.indexOf("1") >= 0) {
-    //       element = "<span id=\"error1\">Beep </span>";
-    //     } else {
-    //       element = i + " ";
-    //     }
-    //     outputString += element;
-    //   }
-    //   return outputString
-    // };
-
-
-    // outputProcessor(userInput)
-    // output = outputProcessor(userInput)
-    // $(".display").text(output)
 
 
 
   });
 });
+
+
+// function outputProcessor(number) {
+//   var outputArray = []
+//   for (var i = 0; i <= userInput; i++) {
+//     var element = i.toString();
+//     var digets = element.split("");
+//     if (digets.indexOf("3") >= 0) {
+//       element = "error3";
+//     } else if (digets.indexOf("2") >= 0) {
+//       element = "error2";
+//     } else if (digets.indexOf("1") >= 0) {
+//       element = "error1";
+//     } else {
+//       element = i;
+//     }
+//     outputArray.push(element);
+//   }
+//   return outputArray
+// };
+
+
+
+
+
+
+
+
+
+
+
+// function numberValidator(number) {
+//   var check = Number.isInteger(number);
+//   console.log(check);
+//   return check;
+// };
